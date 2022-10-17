cd1e86f64a79cf73d1808f16eef10d37f35b19be
raihannfll <113744628+raihannfll@users.noreply.github.com>
2022-10-17 17:00:54 +0700

    Add files via upload

---
+++ Model_latihan1.php
@@ -0,0 +1,25 @@
+<?php
+class Model_Latihan1 extends CI_model 
+{
+    //membuat variable untuk meanmpung nilai
+    public $nilai,$nilai2,$hasil;
+
+    //method penjumlahan
+    public function jumlah($n1 = null, $n2 =null)
+    {
+        $this->nilai1 = $n1;
+        $this->nilai2 = $n2;
+        $this->hasil  =$this->nilai1 + $this->nilai2;
+        return $this->hasil;
+    }
+
+     //method pengurangan
+     
+     public function kurang($n1 = null, $n2 =null)
+     {
+         $this->nilai1 = $n1;
+         $this->nilai2 = $n2;
+         $this->hasil  =$this->nilai1 - $this->nilai2;
+         return $this->hasil;
+     }
+}
